d55fe2f8189800f583c5fc3699429c16cce3d01c
Christopher R. Palmer <crpalmer@gmail.com>
2026-04-25 07:57:31 -0400

    docker.sh: Change the # of threads computation
    This reverts commit bfdd7e3e44b590f0f40a621555b76017eb37e5d6.

--- docker.sh
+++ docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mem=`cat /proc/meminfo | head -1 | cut -c 15-25`
-per_thread=8000000
-threads=`expr '(' $mem + $per_thread - 2 ')' / $per_thread`
+per_thread=4000000
+reserved=16000000
+threads=`expr '(' $mem - $reserved + $per_thread - 2 ')' / $per_thread + 1`
 
 cd /app && ./build_linux.sh -j$threads -dsi -lL
