23aafd02d7ec24f67a44867132a2c52d2d74e630
Christopher R. Palmer <crpalmer@gmail.com>
2026-05-09 11:25:33 -0400

    docker: change # of threads again

--- docker.sh
+++ docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 mem=`cat /proc/meminfo | head -1 | cut -c 15-25`
-per_thread=4000000
+per_thread=2000000
 reserved=16000000
 threads=`expr '(' $mem - $reserved + $per_thread - 2 ')' / $per_thread + 1`
+
+if [ $threads -gt `nproc` ]; then
+    threads=`nproc`
+fi
 
 cd /app && ./build_linux.sh -j$threads -dsi -lL
